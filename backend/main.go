fa0b7c0b7699a721a5440e51e579ae1b69b417b0
Valentin L <valentin@bitmask.it>
2024-07-16 16:23:43 +0300

    moved env example && use config for backend port

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/bitmaskit/notifications/backend/api"
 	"github.com/bitmaskit/notifications/kafka"
 )
 
+const env = ".env"
+
+var port string
+
 func main() {
-	//api := backend.API{kafka.Kafka{}}
+	if err := godotenv.Load(env); err != nil {
+		log.Fatalf("Failed to load env: %v", err)
+	}
+	if port = os.Getenv("BACKEND_PORT"); port == "" {
+		port = "8080"
+	}
+
 	api := api.API{Kafka: kafka.Kafka{}}
+
 	http.HandleFunc("/message", api.PostHandler)
 
-	log.Println("Starting server on :8001")
-	if err := http.ListenAndServe(":8001", nil); err != nil {
-		log.Fatalln("Error starting server: ", err)
+	log.Println("Starting server on port: ", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalln("Error running the server", err)
 	}
 }
