ab2d410e4c087ae4d0298011e5fbd3d076be1547
Valentin L <valentin@bitmask.it>
2024-07-16 18:58:35 +0300

    frontend config refactored

--- frontend/config/config.go
+++ frontend/config/config.go
@@ -1,9 +1,37 @@
 package config
 
-func New(backendAddr string) *Config {
-	return &Config{backendAddr}
+import (
+	"errors"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const env = ".env"
+
+var ErrFrontendPortNotSet = errors.New("FRONTEND_PORT is not set")
+var ErrBackendHostNotSet = errors.New("BACKEND_HOST is not set")
+
+type FrontendConfig struct {
+	Port        string
+	BackendAddr string
 }
 
-type Config struct {
-	BackendAddr string
+func Load() (*FrontendConfig, error) {
+	if err := godotenv.Load(env); err != nil {
+		return nil, err
+	}
+
+	port := os.Getenv("FRONTEND_PORT")
+	if port == "" {
+		return nil, ErrFrontendPortNotSet
+	}
+	backendHost := os.Getenv("BACKEND_HOST")
+	if backendHost == "" {
+		return nil, ErrBackendHostNotSet
+	}
+	return &FrontendConfig{
+		Port:        port,
+		BackendAddr: backendHost,
+	}, nil
 }
--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,44 +1,25 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/bitmaskit/notifications/frontend/api"
 	"github.com/bitmaskit/notifications/frontend/config"
-
-	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
-
-const env = ".env"
-
-var (
-	backendHost string
-	port        string
-)
-
-func init() {
-	if err := godotenv.Load(env); err != nil {
-		log.Fatalf("Failed to load env: %v", err)
-	}
-	if port = os.Getenv("FRONTEND_PORT"); port == "" {
-		log.Fatalln("FRONTEND_PORT is not set")
-	}
-	if backendHost = os.Getenv("BACKEND_HOST"); backendHost == "" {
-		log.Fatalln("BACKEND_HOST is not set")
-	}
-}
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalln("Failed to load config: ", err)
+	}
 
-	api := api.New(config.New(backendHost))
+	api := api.New(cfg)
 
 	http.HandleFunc("GET /", api.IndexHandler)
 	http.HandleFunc("POST /", api.PostHandler)
 
-	log.Println("Starting server on port: ", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	log.Println("Starting server on port: ", cfg.Port)
+	if err := http.ListenAndServe(":"+cfg.Port, nil); err != nil {
 		log.Fatalln("Error running the server", err)
 	}
 }
